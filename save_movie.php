7c91f1fdb9c08e2a9d1c6580145974871f06a8e6
Jakooth <ivan.k.vladimirov@gmail.com>
2015-10-23 01:05:30 +0300

    Save object get authors
    Saving all objects and get authors complete.

--- save_movie.php
+++ save_movie.php
@@ -1,5 +1,4 @@
 <?php
-$related_arr = array ();
 $php_fortag_worlddate = isset ( $php_fortag ['worldDate'] ) ? "'{$php_fortag ['worlddDate']}'" : "null";
 $php_fortag_time = isset ( $php_fortag ['time'] ) ? "'{$php_fortag ['time']}'" : "null";
 
@@ -18,84 +17,7 @@ if (! $related_result) {
 	goto end;
 }
 
-if ($php_fortag ['stickers']) {
-	foreach ( $php_fortag ['stickers'] as $value ) {
-		$id = isset ( $value ['id'] ) ? "'{$value['id']}'" : "null";
-		
-		$related_sql = "INSERT INTO for_rel_stickers
-							(tag_id, sticker_id)
-						VALUES
-							($tag_last, $id);";
-		
-		$related_result = mysqli_query ( $link, $related_sql );
-		
-		if (! $related_result) {
-			goto end;
-		}
-	}
-}
-
-if ($php_fortag ['genres']) {
-	foreach ( $php_fortag ['genres'] as $value ) {
-		$id = isset ( $value ['id'] ) ? "'{$value['id']}'" : "null";
-		
-		$related_sql = "INSERT INTO for_rel_genres
-							(tag_id, genre_id)
-						VALUES
-							($tag_last, $id);";
-		
-		$related_result = mysqli_query ( $link, $related_sql );
-		
-		if (! $related_result) {
-			goto end;
-		}
-	}
-}
-
-if ($php_fortag ['serie']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['serie'] );
-}
-
-if ($php_fortag ['similar']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['similar'] );
-}
-
-if ($php_fortag ['cast']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['cast'] );
-}
-
-if ($php_fortag ['director']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['director'] );
-}
-
-if ($php_fortag ['writer']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['director'] );
-}
-
-if ($php_fortag ['camera']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['director'] );
-}
-
-if ($php_fortag ['music']) {
-	$related_arr = array_merge ( $related_arr, $php_fortag ['director'] );
-}
-
-if (count ( $related_arr ) > 0) {
-	foreach ( $related_arr as $value ) {
-		$id = isset ( $value ['id'] ) ? "'{$value['id']}'" : "null";
-		
-		$related_sql = "INSERT INTO for_rel_relative
-							(tag_id, related_tag_id)
-						VALUES
-							($tag_last, $id);";
-		
-		$related_result = mysqli_query ( $link, $related_sql );
-		
-		if (! $related_result) {
-			goto end;
-		}
-	}
-}
+include ('save_game.php');
 
 end:
 
